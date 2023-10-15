95504865a512bdbada8cdb6d9a75b3fe488cfc76
Alice Sera <sera.alice@seznam.cz>
2023-10-15 22:42:56 +0200

    Primary_Table_Alice_Sera.sql

---
+++ Primary_Table_Alice_Sera.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS t_Alice_Sera_project_SQL_primary_final AS (
+SELECT 
+	cpc.name AS food_category,
+	cpc.price_value,
+	cpc.price_unit,
+	cp.value AS food_price,
+	year(cp.date_from) AS year_relevant_to_price,
+	cpib.name AS industry_name,
+	cpay.payroll_quarter,
+	cpay.payroll_year,
+	cpay.value AS aver_wage
+FROM czechia_price cp 
+JOIN czechia_payroll cpay 
+	ON year(cp.date_from) = cpay.payroll_year
+LEFT JOIN czechia_payroll_industry_branch cpib 
+	ON cpay.industry_branch_code  = cpib.code 
+LEFT JOIN czechia_price_category cpc 
+	ON cpc.code = cp.category_code 
+WHERE 
+	cpay.value_type_code = 5958)
+;
+
+
+
+
+
+
