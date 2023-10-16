b08031228737cc3576c46eca81f8cc9d7f4e3389
Alice Sera <sera.alice@seznam.cz>
2023-10-16 14:23:34 +0200

    Primary_Table_Alice_Sera.sql

--- Primary_Table_Alice_Sera.sql
+++ Primary_Table_Alice_Sera.sql
@@ -8,7 +8,8 @@ SELECT
 	cpib.name AS industry_name,
 	cpay.payroll_quarter,
 	cpay.payroll_year,
-	cpay.value AS aver_wage
+	cpay.value AS aver_wage,
+	cpu.name AS currency
 FROM czechia_price cp 
 JOIN czechia_payroll cpay 
 	ON year(cp.date_from) = cpay.payroll_year
@@ -16,11 +17,11 @@ LEFT JOIN czechia_payroll_industry_branch cpib
 	ON cpay.industry_branch_code  = cpib.code 
 LEFT JOIN czechia_price_category cpc 
 	ON cpc.code = cp.category_code 
+LEFT JOIN czechia_payroll_unit cpu
+	ON cpu.code = cpay.unit_code
 WHERE 
-	cpay.value_type_code = 5958)
+	cpay.value_type_code = 5958 AND cpay.calculation_code = 200)
 ;
-
-
 
 
 
