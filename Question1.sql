66ae97ff1fa17ac671d3d9a4520606134d3bc27f
Alice Sera <sera.alice@seznam.cz>
2023-10-20 20:44:20 +0200

    Question1.sql

--- Question1.sql
+++ Question1.sql
@@ -1,22 +1,50 @@
--- 1. tabulka srovnávací min. a max. rok
+-- 1. tabulka srovnávací min. a max. rok, odvětví, ve kterých mzdy neklesly
 
 SELECT
-    industry_name,
-    aver_wage,
-    MIN(payroll_year) AS start_year,
-    MAX(payroll_year) AS end_year
-FROM t_alice_sera_project_sql_primary_final pt
-WHERE industry_name IS NOT NULL
-GROUP BY industry_name
-HAVING MIN(aver_wage) <= MAX(aver_wage);
+	industry_name,
+	AVG(aver_wage) AS average_wage,
+	MIN(payroll_year) AS start_year,
+	MAX(payroll_year) AS end_year
+FROM
+	t_alice_sera_project_sql_primary_final pt
+WHERE
+	industry_name IS NOT NULL
+GROUP BY
+	industry_name
+HAVING
+	MIN(aver_wage) <= MAX(aver_wage);
 
 
 -- tabulka k detailnější analýze
+
 SELECT
-    industry_name,
-    payroll_year,
-    AVG(aver_wage) AS average_wage
-FROM t_alice_sera_project_sql_primary_final pt
-WHERE industry_name IS NOT NULL
-GROUP BY industry_name, payroll_year
-;
+	industry_name,
+	AVG(aver_wage) AS average_wage,
+	MIN(payroll_year) AS start_year,
+	MAX(payroll_year) AS end_year
+FROM
+	t_alice_sera_project_sql_primary_final pt
+WHERE
+	industry_name IS NOT NULL
+GROUP BY
+	industry_name,
+	payroll_year
+HAVING
+	MIN(aver_wage) <= MAX(aver_wage);
+	
+-- odvětví, ve kterých mzdy klesaly
+
+SELECT
+	industry_name,
+	AVG(aver_wage) AS average_wage,
+	MIN(payroll_year) AS start_year,
+	MAX(payroll_year) AS end_year
+FROM
+	t_alice_sera_project_sql_primary_final pt
+WHERE
+	industry_name IS NOT NULL
+GROUP BY
+	industry_name,
+	payroll_year
+HAVING
+	MIN(aver_wage) > MAX(aver_wage);
